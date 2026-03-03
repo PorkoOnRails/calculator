2c73a0a9e0e2fba3b332aae10b9ad9db6cd91893
Porko <PorkoOnRails@hotmail.com>
2026-03-03 00:51:39 -0300

    Another great work doing evething in the way i thing and undestand without copy paste code from the internet

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//Function to test the click in the buttons
-function inputPlus() {
-    alert("apertado");
-}
-
 //Create a shortcut to use the value of the numbers
 var valueNum0 = document.getElementById("number0").value;
 var valueNum1 = document.getElementById("number1").value;
@@ -14,13 +9,87 @@ var valueNum6 = document.getElementById("number6").value;
 var valueNum7 = document.getElementById("number7").value;
 var valueNum8 = document.getElementById("number8").value;
 var valueNum9 = document.getElementById("number9").value;
-
 var resultNumber = document.getElementById("numberResult");
 
 //Function to change the contend of the result that's display
-function displayResult(num1, numb2) {
+//The function called .split i don't understand how it works but tomorrow i will learn it and
+//Change if necessary, the corrector who give me this 'tip' that resolve this problem
+function displayResult() {
+    if (resultNumber.textContent.includes("+")) {
+        result = previusresult + Number(resultNumber.textContent.split("+")[1]);
+    } else if (resultNumber.textContent.includes("-")) {
+        result = previusresult - Number(resultNumber.textContent.split("-")[1]);
+    } else if (resultNumber.textContent.includes("*")) {
+        result = Number(previusresult) * Number(resultNumber.textContent.split("*")[1]);
+    } else if (resultNumber.textContent.includes("/")) {
+        result = Number(previusresult) / Number(resultNumber.textContent.split("/")[1]);
+    }
+    
+    return resultNumber.textContent = result;
+}
 
-    result = num1 + numb2;
+//Declaring the variables to be used in the functions
+var previusresult = 0;
 
-    resultNumber.textContent = "Resultado da operação é" + result;
+//Functions to change the operator
+function sum() {
+    previusResult();
+    resultNumber.textContent = "+";
+}
+function minus() {
+    previusResult();
+    resultNumber.textContent = "-";
+}
+function multiply() {
+    previusResult();
+    resultNumber.textContent = "*";
+}
+function divided() {
+    previusResult();
+    resultNumber.textContent = "/";
+}
+
+//Change the variable previusresult to the current result
+function previusResult() {
+    return previusresult = Number(resultNumber.textContent);
+}
+
+//Functions to clear the result
+function clearResult() {
+    resultNumber.textContent = "0";
+}
+function clearResultAll() {
+    resultNumber.textContent = "";
+}
+
+//Functions to add the numbers to be calcuted in the worst way
+function addNumberToBeCalcuted1() {
+    resultNumber.textContent += "1";
+}
+function addNumberToBeCalcuted2() {
+    resultNumber.textContent += "2";
+}
+function addNumberToBeCalcuted3() {
+    resultNumber.textContent += "3";
+}
+function addNumberToBeCalcuted4() {
+    resultNumber.textContent += "4";
+}
+function addNumberToBeCalcuted5() {
+    resultNumber.textContent += "5";
+}
+function addNumberToBeCalcuted6() {
+    resultNumber.textContent += "6";
+}
+function addNumberToBeCalcuted7() {
+    resultNumber.textContent += "7";
+}
+function addNumberToBeCalcuted8() {
+    resultNumber.textContent += "8";
+}
+function addNumberToBeCalcuted9() {
+    resultNumber.textContent += "9";
+}
+function addNumberToBeCalcuted0() {
+    resultNumber.textContent += "0";
 }
