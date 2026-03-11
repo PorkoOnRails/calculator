c1e388170079e0bd4a10c4eda9b994ef2ee94469
Porko <PorkoOnRails@hotmail.com>
2026-03-10 23:03:13 -0300

    Work on css and copy some codes because I spend some many time trying to make the things look better and I want to save that time. Also add the percentage button just to fit beter the grid!

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ var valueNum9 = document.getElementById("number9").value;
 var resultNumber = document.getElementById("numberResult");
 
 //Function to change the contend of the result that's display
-//The function called .split i don't understand how it works but tomorrow i will learn it and
-//Change if necessary, the corrector who give me this 'tip' that resolve this problem
+//The function called .split to split the string in the operator and the number to be calcuted, then it uses the operator to know which operation to do and return the result
 function displayResult() {
     if (resultNumber.textContent.includes("+")) {
         result = previusresult + Number(resultNumber.textContent.split("+")[1]);
@@ -24,8 +23,19 @@ function displayResult() {
     } else if (resultNumber.textContent.includes("/")) {
         result = Number(previusresult) / Number(resultNumber.textContent.split("/")[1]);
     }
-    
+    //Just make this to fit and look better
+        else if (resultNumber.textContent.includes("%")) {
+        result = Number(previusresult) * (Number(resultNumber.textContent.split("%")[1]) / 100);
+    }
+
     return resultNumber.textContent = result;
+}
+
+
+//Function to erase the last number is with .slice(0, -1) because is a string not a number otherwise I would use .pop()
+function eraseLast() {
+    previusresult = resultNumber.textContent.slice(0, -1);
+    resultNumber.textContent = previusresult;
 }
 
 //Declaring the variables to be used in the functions
@@ -47,6 +57,10 @@ function multiply() {
 function divided() {
     previusResult();
     resultNumber.textContent = "/";
+}
+function percentage() {
+    previusResult();
+    resultNumber.textContent = "%";
 }
 
 //Change the variable previusresult to the current result
